00262c037820084b66cbb7c7c03cbcea518251e3
Thomas Ankcorn <thomasankcorn@gmail.com>
2026-06-25 22:24:30 +0100

    Migrate styling to Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [cloudflare(), react()],
+  plugins: [cloudflare(), tailwindcss(), react()],
   resolve: {
     alias: {
       turndown: "turndown/lib/turndown.browser.es.js",
